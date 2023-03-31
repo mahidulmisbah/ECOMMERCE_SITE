e020df90e40c857a23202a19cc632f112a1513cd
Kazi Md. Mahidul Islam <mahidul.cse.du@gmail.com>
2023-03-31 20:22:59 +0000

    initial commit

---
+++ config/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import colors from "colors";
+
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URL);
+        console.log(`MongoDB Connected Successfully ${conn.connection.host}`.bgGreen.white);
+    } catch (error) {
+        console.log(`MongoDB Error on ${error}`.bgRed.white);
+    }
+};
+
+export default connectDB; 
---
+++ server.js
@@ -0,0 +1,35 @@
+import express from "express";
+import colors from "colors";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+
+
+// config env
+dotenv.config();
+
+// database connection 
+connectDB();
+
+// rest  
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+//rest api
+app.get("/", (req,res) => {
+    res.send("<h1 style=\"color: red; text-align: center; font-size: 40px;\">This is my world</h1>");
+});
+
+
+//PORT configer 
+const PORT = process.env.PORT || 8020;
+
+// run listen
+
+app.listen(PORT, () => {
+    console.log(`Server running on ${PORT}`.bgYellow.white);
+})
